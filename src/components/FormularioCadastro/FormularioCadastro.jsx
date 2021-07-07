f1b6a0e42a3e048470451d45eb11e23be78fa102
kirestein <erik.proenca2011@gmail.com>
2021-07-07 20:23:30 -0300

    Manipulando o JS para fazer o aplicativo salvar as notas

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,24 +3,38 @@ import "./estilo.css"
 class FormularioCadastro extends Component{
 
     constructor() {
+        super();
         this.titulo = "";
+        this.texto = "";
     }
-    handleMudancaTitulo(evento) {
+    _handleMudancaTitulo(evento) {
+        evento.stopPropagation();
         this.titulo = evento.target.value;
+    }
+    _handleMudancaTexto(evento) {
+        evento.stopPropagation();
+        this.texto = evento.target.value;
+    }
+    _criarNota(evento) {
+        evento.preventDefault();
+        evento.stopPropagation();
     }
     render(){
         return(
-            <form className="form-cadastro">
+            <form className="form-cadastro"
+                onSubmit={this._criarNota.bind(this)}
+            >
                 <input 
                  type="text"
                  placeholder="Título"
                  className="form-cadastro_input" 
-                 onChange={this.handleMudancaTitulo}
+                 onChange={this._handleMudancaTitulo.bind(this)}
                 />
                 <textarea
                  rows={15}
                  placeholder="Escreva sua nota..."
                  className="form-cadastro__input" 
+                 onChange={this._handleMudancaTexto.bind(this)}
                 />
                 <button className="form-cadastro_input form-cadastro_submit">
                     Criar nota
